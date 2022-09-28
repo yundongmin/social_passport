4b4db8a13ca435c30b46d3d9d61ad10119651e2a
Dongmin <kantera16@naver.com>
2022-09-28 09:23:17 +0900

    first commit

---
+++ backend/passport.js
@@ -0,0 +1,61 @@
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const GithubStrategy = require("passport-github2").Strategy;
+const FacebookStrategy = require("passport-facebook").Strategy;
+const passport = require("passport");
+
+const GOOGLE_CLIENT_ID =
+  "9267121280-vd40sk2t2e5nc2orjd9j5j2iavin9256.apps.googleusercontent.com";
+const GOOGLE_CLIENT_SECRET = "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";
+
+GITHUB_CLIENT_ID = "7dd1ceac219cd5736264";
+GITHUB_CLIENT_SECRET = " dffadfadfafs";
+
+FACEBOOK_APP_ID = "686603422419086";
+FACEBOOK_APP_SECRET = "61b05988bcd2432f708bb3ee3a347902";
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+      callbackURL: "/auth/google/callback",
+    },
+    function (accessToken, refreshToken, profile, done) {
+      done(null, profile);
+    }
+  )
+);
+
+passport.use(
+  new GithubStrategy(
+    {
+      clientID: GITHUB_CLIENT_ID,
+      clientSecret: GITHUB_CLIENT_SECRET,
+      callbackURL: "/auth/github/callback",
+    },
+    function (accessToken, refreshToken, profile, done) {
+      done(null, profile);
+    }
+  )
+);
+
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: FACEBOOK_APP_ID,
+      clientSecret: FACEBOOK_APP_SECRET,
+      callbackURL: "/auth/facebook/callback",
+    },
+    function (accessToken, refreshToken, profile, done) {
+      done(null, profile);
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
---
+++ client/src/data.js
@@ -0,0 +1,23 @@
+export const posts = [
+  {
+    id: 1,
+    title: "Google Login",
+    img: "https://images.pexels.com/photos/7963572/pexels-photo-7963572.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    desc: "successs",
+    longDesc: " 생활코딩 만세 ",
+  },
+  {
+    id: 2,
+    title: "Facebook Login",
+    img: "https://images.pexels.com/photos/1742370/pexels-photo-1742370.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    desc: "erro",
+    longDesc: "생활코딩 악마야",
+  },
+  {
+    id: 3,
+    title: "Github Login",
+    img: "https://images.pexels.com/photos/1161547/pexels-photo-1161547.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    desc: "sucess",
+    longDesc: "외국인 최고",
+  },
+];
